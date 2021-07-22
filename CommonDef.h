2cb90b639cf95138f2b40b65819d67321cf84873
BennettCook <bcook845@live.com>
2021-07-22 16:10:24 -0400

    works with the controller and stepper

---
+++ CommonDef.h
@@ -0,0 +1,15 @@
+/// file: CommonDef.h
+/// author: Bennett Cook
+/// date: 07-05-2020
+/// description: a place to put common program definitions. 
+/// update: 05-27-2021: add help light on time constants
+
+
+// header guard
+#ifndef COMMONDEF_H
+#define COMMONDEF_H
+
+const int STEPPER_ENABLE = 29; 
+const int STEPPER_DIRECTION = 25; 
+
+#endif // end header guard
--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include "Rp4bPwm.h"
 #include <chrono>
 #include <thread>
+#include "wiringPi.h"
+#include "CommonDef.h"
 
 using namespace std;
 
@@ -10,22 +12,39 @@ using namespace std;
 // g++ -g -opwm main.cpp Rp4bPwm.cpp -lpthread 
 int main(int argc, char* args[]){
 
-   unsigned dc = 10;
+
+   wiringPiSetup();                     // initialize wiringPi.  
+   pinMode(STEPPER_ENABLE, OUTPUT);     // set STEPPER_ENABLE to output
+   pinMode(STEPPER_DIRECTION, OUTPUT);  // set STEPPER_DIRECTION to output
+
+   digitalWrite(STEPPER_ENABLE, LOW);
+   digitalWrite(STEPPER_DIRECTION, HIGH);
+
+   unsigned dc = 50;
    Rp4bPwm pwm(PwmNumber::Pwm1);
-   pwm.SetFrequenceHz(1000);
+   pwm.SetFrequenceHz(100);
    pwm.SetDutyCyclePercent(dc);
    pwm.Enable(true);
 
+   digitalWrite(STEPPER_ENABLE, HIGH);
+   
 
-   while(dc < 100) {
-      pwm.SetDutyCyclePercent(dc+=10);
-      this_thread::sleep_for(chrono::milliseconds(200));
-   } 
+   this_thread::sleep_for(chrono::seconds(10));
 
-   while(dc > 0) {
-      pwm.SetDutyCyclePercent(dc-=10);
-      this_thread::sleep_for(chrono::milliseconds(200));
-   } 
+   // while(dc < 100) {
+   //    pwm.SetDutyCyclePercent(dc+=10);
+   //    this_thread::sleep_for(chrono::milliseconds(200));
+   // } 
+
+   // while(dc > 0) {
+   //    pwm.SetDutyCyclePercent(dc-=10);
+   //    this_thread::sleep_for(chrono::milliseconds(200));
+   // } 
+
+   pwm.Enable(false);
+
+   digitalWrite(STEPPER_ENABLE, LOW);
+   digitalWrite(STEPPER_DIRECTION, LOW);
 
    return 0;
 } // end main
