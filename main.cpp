9a645a7348568097c165885df164d5595a76f866
BennettCook <bcook845@live.com>
2021-09-13 15:13:39 -0400

    more debug

--- main.cpp
+++ main.cpp
@@ -8,8 +8,7 @@
 
 using namespace std;
 
-
-// g++ -g -opwm main.cpp Rp4bPwm.cpp -lpthread 
+//g++ -g -opwm main.cpp Rp4bPwm.cpp -lwiringPi -lpthread
 int main(int argc, char* args[]){
 
 
@@ -17,32 +16,38 @@ int main(int argc, char* args[]){
    pinMode(STEPPER_ENABLE, OUTPUT);     // set STEPPER_ENABLE to output
    pinMode(STEPPER_DIRECTION, OUTPUT);  // set STEPPER_DIRECTION to output
 
-   digitalWrite(STEPPER_ENABLE, LOW);
+   digitalWrite(STEPPER_ENABLE, HIGH);
    digitalWrite(STEPPER_DIRECTION, HIGH);
+
+   // this_thread::sleep_for(chrono::seconds(10)); // pause
 
    unsigned dc = 50;
    Rp4bPwm pwm(PwmNumber::Pwm1);
-   pwm.SetFrequenceHz(100);
+   pwm.SetFrequenceHz(1600);
    pwm.SetDutyCyclePercent(dc);
-   pwm.Enable(true);
-
-   digitalWrite(STEPPER_ENABLE, HIGH);
-   
-
-   this_thread::sleep_for(chrono::seconds(10));
-
-   // while(dc < 100) {
-   //    pwm.SetDutyCyclePercent(dc+=10);
-   //    this_thread::sleep_for(chrono::milliseconds(200));
-   // } 
-
-   // while(dc > 0) {
-   //    pwm.SetDutyCyclePercent(dc-=10);
-   //    this_thread::sleep_for(chrono::milliseconds(200));
-   // } 
-
    pwm.Enable(false);
 
+   size_t tim = 16500;
+
+   for(auto i = 0; i < 1; ++i){
+
+      digitalWrite(STEPPER_DIRECTION, HIGH); // extend
+      pwm.Enable(true);
+      this_thread::sleep_for(chrono::milliseconds(tim)); // 4650
+   
+      pwm.Enable(false);
+      digitalWrite(STEPPER_DIRECTION, LOW); // retract
+
+      this_thread::sleep_for(chrono::seconds(1));
+
+      pwm.Enable(true);
+      this_thread::sleep_for(chrono::milliseconds(tim-50)); // 4750
+      pwm.Enable(false);
+
+      this_thread::sleep_for(chrono::seconds(1)); // pause 
+   } // end for 
+
+   pwm.Enable(false);
    digitalWrite(STEPPER_ENABLE, LOW);
    digitalWrite(STEPPER_DIRECTION, LOW);
 
