0d58ef2ec3bae745fee79d189d431c88fed3abe2
BennettCook <bcook845@live.com>
2022-06-15 13:54:42 -0400

    add seonds y-axis

--- chart/main.cpp
+++ chart/main.cpp
@@ -31,7 +31,14 @@ int main(int argc, char* argv[]){
    sqlite3 *db;
    sqlite3_stmt *stmt;
 
-   float max = -100.0f; // for max temp in query
+   // use these to customize the yaxis ranges
+   float maxTemp = -100.0f; // for max temp in query
+   float maxLight = -100.0f; // for max light in query
+
+   const float y1MaxPadding = 5;
+   const float y2MaxPadding = 100;
+   const float y2ticsCount = 6;
+
  
    // open db use full path 
    int rc = sqlite3_open(dbFullPath.c_str(), &db);
@@ -72,11 +79,19 @@ int main(int argc, char* argv[]){
                string light = ToStdStr(sqlite3_column_text(stmt, 3));
                out << timestamp << "," << temperature << "," << humidity << "," << light << endl;
 
-               // convert and compare to get temp max in query
+               // convert and compare to get the temp max in query
                // and ignore on error 
                try {
-                  float value = lexical_cast<float>(temperature);
-                  if(value > max) max = value;
+                  float tempVal = lexical_cast<float>(temperature);
+                  if(tempVal > maxTemp) maxTemp = tempVal;
+               }
+               catch(const bad_lexical_cast &){} 
+
+               // convert and compare to get the light max in query
+               // and ignore on error 
+               try {
+                  float lightVal = lexical_cast<float>(light);
+                  if(lightVal > maxLight) maxLight = lightVal;
                }
                catch(const bad_lexical_cast &){} 
 
@@ -107,36 +122,15 @@ int main(int argc, char* argv[]){
       // through a linux pipe
       
       GnuPlot gp;
-   /*
-      // multi plot, with stacked plots
+
+      // single plot with three traces
       gp("set terminal pngcairo size 800,400");
       gp("set output 'chart.png'");
-      gp("set datafile separator ','");
-      gp("set key autotitle columnhead");
       gp("set timefmt '%Y-%m-%d %H:%M:%S'");
-      gp("set multiplot layout 2, 1 title 'Garage Temperature and Humidity' font ',14'");
-      gp("set title 'Temperature in degF'");
-      gp("set xdata time");
-      gp("set format x \"%H:%M\"");
-      gp("set key off");
-      gp("set grid ytics lt 0 lw 1 lc rgb '#bbbbbb'");
-      gp("set grid xtics lt 0 lw 1 lc rgb '#bbbbbb'");
-      gp("plot [] [0:100] 'readings.txt' using 1:2 w l");
-      gp("set title '% Relative Humidity'");
-      gp("set xdata time");
-      gp("set format x \"%H:%M\"");
-      gp("set key off");
-      gp("set grid ytics lt 0 lw 1 lc rgb '#bbbbbb'");
-      gp("set grid xtics lt 0 lw 1 lc rgb '#bbbbbb'");
-      gp("plot [] [0:100] 'readings.txt' using 1:3 w l");
-   */
-      // single plot with two traces
-      gp("set terminal pngcairo size 800,400");
-      gp("set output 'chart.png'");
       gp("set style line 1 lc 'dark-red' lw 2");
       gp("set style line 2 lc 'blue' lw 2");
-      gp("set style line 3 lc 'yellow' lw 2");
-      gp("set title 'Chicken Coop Temperature, Humidity, Light' font ',12'");
+      gp("set style line 3 lc 'dark-green' lw 2");
+      gp("set title 'Chicken Coop Temperature, Humidity, and Light' font ',12'");
       gp("set border 3 lw 2");
       gp("set key autotitle columnhead");
       gp("set key bottom right");
@@ -144,35 +138,73 @@ int main(int argc, char* argv[]){
       gp("set grid");
       gp("set tics font ',10'");
       gp("set tics nomirror");
-      gp("set ylabel 'degF or %' font ',10'");
       gp("set xlabel 'hour:minute' font ',10'");
       gp("set xlabel offset -1");
-      gp("set datafile separator ','");
       gp("set xdata time");
-      gp("set timefmt '%Y-%m-%d %H:%M:%S'");
-      gp("set format x '%H:%M'");
-      gp("set ytics 0,10,100");
       gp("set xtics rotate by 45 offset -2.3,-1.2");
-      string plotString = "plot [] [0:placeholder] 'readings.txt' using 1:2 w l ls 1,'readings.txt' using 1:3 w l ls 2,'readings.txt' using 1:4 w l ls 3";
+      gp("set format x '%H:%M'");
+      gp("set ylabel 'degF and %' font ',10'");
+      gp("set ytics 0,10");
+      gp("set ytics nomirror");
+      gp("set y2label 'light (lx)' font ',10'");
+      gp("set y2tics 0,2000");
+      gp("set datafile separator ','");
 
-      // adjust the y max range up if needed 
-      string rangeMax = "100";
-      if(max >= 100.0f){
+      // used to customize the
+      string y1rangeStr = "set yrange [0:placeholder1]";
+      string y2rangeStr = "set y2range [0:placeholder2]";
+      string y2ticsStr = "set y2tics 0,placeholder3";
+
+      string plotStr = "plot 'readings.txt' using 1:2 w l ls 1 axis x1y1, 'readings.txt' using 1:3 w l ls 2 axis x1y1, 'readings.txt' using 1:4 w l ls 3 axis x1y2";
+
+      // adjust the y1 axis range up if needed 
+      string range1Max = "100";
+      if(maxTemp >= 100.0f){
          
          // ignore error and use default rangeMax
          try {
-            rangeMax = lexical_cast<string>(static_cast<int>(max + 5));
+            range1Max = lexical_cast<string>(static_cast<int>(maxTemp + y1MaxPadding));
          }
          catch(const bad_lexical_cast &){} 
 
       } // end if 
+      replace_first(y1rangeStr, "placeholder1", range1Max);
 
-      replace_first(plotString, "placeholder", rangeMax);
+      // adjust the y2 axis range up if needed 
+      string range2Max = "10000";
+      if(maxLight >= 10000.0f){
+         
+         // ignore error and use default range2Max
+         try {
+            range2Max = lexical_cast<string>(static_cast<int>(maxLight + y2MaxPadding));
+         }
+         catch(const bad_lexical_cast &){} 
 
-      gp(plotString.c_str());
+      } // end if 
+      replace_first(y2rangeStr, "placeholder2", range2Max);
+
+      // set the my2tic frequncy
+      int y2tics = static_cast<int>((maxLight + y2MaxPadding) / y2ticsCount);
+      string y2ticsFreq;
+      // ignore error and use default range2Max
+      try {
+         y2ticsFreq = lexical_cast<string>(static_cast<int>(y2tics));
+      }
+      catch(const bad_lexical_cast &){} 
+      replace_first(y2ticsStr, "placeholder3", y2ticsFreq);
+
+      gp(y2ticsStr.c_str());
+      gp(y1rangeStr.c_str());
+      gp(y2rangeStr.c_str());
+      gp(plotStr.c_str());
 
       } // end if 
 
    return ret;
 } // end main
 
+/*
+
+
+
+*/
